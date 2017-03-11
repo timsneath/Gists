b97b68cacbc0f02b55bae02a4df3f2e40534a0b0
Tim Sneath <tim@sneath.org>
2017-03-11 14:45:43 -0800

    Added comment for private keys

--- GistsVSIX/Properties/APIKeys.cs
+++ GistsVSIX/Properties/APIKeys.cs
@@ -8,6 +8,7 @@ namespace Microsoft.VisualStudio.Extensions.Gists.Properties
 {
     public static partial class APIKeys
     {
+        // Production VSIX has values here. You can get your own values from github.com
         public static string GistClientID = "";
         public static string GistClientSecret = "";
     }
